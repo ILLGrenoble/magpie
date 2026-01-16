c2177d8aa05ae479b4c1061a90701b0b4d2d2beb
Tobias WEBER <tweber@ill.fr>
2026-01-16 07:05:29 +0100

    tlibs: small corrections

--- tlibs2/tools/script/src/llasm.cpp
+++ tlibs2/tools/script/src/llasm.cpp
@@ -208,6 +208,7 @@ std::string LLAsm::get_type_name(SymbolType ty)
 		case SymbolType::VOID: return "void";
 		case SymbolType::COMP: return "i8*";	// pointer to memory block
 		//case SymbolType::FUNC: return "func";	// TODO: function pointers
+		default: return "invalid";
 	}
 
 	return "invalid";
@@ -303,6 +304,8 @@ std::size_t LLAsm::get_bytesize(t_astret sym)
 		}
 		case SymbolType::FUNC:
 			return sizeof(void*);
+		default:
+			return 0;
 	}
 
 	return 0;
